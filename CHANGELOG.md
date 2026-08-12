8043c679c7ba9ad87b202f2ec6312f637d5a3f71
semantic-release <semantic-release>
2026-08-12 13:16:21 +0000

    chore(release): 1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 # CHANGELOG
 
 <!-- version list -->
+
+## v1.0.0 (2026-08-12)
+
+- Initial Release
--- src/baysil/__init__.py
+++ src/baysil/__init__.py
@@ -6,4 +6,4 @@
 Bayesian Sequential Inference Library
 """
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
